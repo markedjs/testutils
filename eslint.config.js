6e1f5b9017d8d8583b63c937ffbd34c26f9a87be
Tony Brix <tony@brix.ninja>
2024-04-10 22:28:04 -0600

    chore: update eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+];
